d6312dd6cf4faacb365c3966e12c96b274a330b7
Fulei <emil.fulei@gmail.com>
2023-03-01 08:46:44 +0100

    xml tree

--- python_fuzzer/parsers/document_parser.py
+++ python_fuzzer/parsers/document_parser.py
@@ -1,6 +1,6 @@
 import os
 from typing import List, Any
-
+from xml.etree.ElementTree import Element, ElementTree, parse, ParseError
 
 if __name__ == "__main__":
     from input_parser import InputParser
@@ -13,17 +13,31 @@ class DocumentParser(InputParser):
         self.path: str = path
         self.verbose: bool = verbose
 
-    def load_seed(self) -> List[Any]:
+    def load_corpus(self) -> List[ElementTree]:
         """
-        TODO write this.
+        Load OIOUBL documents from the fuzzer or example documents into a XML tree structure.
+        :return: The corpus, which is the input to the fuzzer.
         """
-        seed = []
+        corpus = []
 
         # Find all files in folder
-        files = [file for file in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, file))]
-        return seed
+        files: List[str] = [file for file in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, file))]
+        # Filter for XML files
+        files: List[str] = [file for file in files if file.endswith(".xml")]
+
+        for file in files:
+            path: str = os.path.join(self.path, file)
+            if self.verbose:
+                print(f"Parsing xml file: {file}")
+            try:
+                tree: ElementTree = parse(path)
+                corpus.append(tree)
+            except ParseError as err:
+                print(f"Error Parsing the OIOUBL document: {err}")
+        return corpus
 
 
 if __name__ == "__main__":
     # Just for testing this class
+
     pass
