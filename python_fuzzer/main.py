e6a8bc54128b5bcc2a50fd695285251466cd3c51
Fulei <emil.fulei@gmail.com>
2023-04-12 09:43:02 +0200

    made a ddos method

--- python_fuzzer/main.py
+++ python_fuzzer/main.py
@@ -3,7 +3,7 @@ from loggers import FeedbackLogger
 from fuzzers import RaspFuzzer, GreyboxFuzzer
 from runners import RaspRunner
 from mutators import DocumentMutator, StructureMutator
-from scheduler import  PowerSchedule
+from scheduler import PowerSchedule
 from utils import Seed, Ddos
 
 import os
@@ -11,51 +11,56 @@ import argparse
 from typing import List
 from xml.etree.ElementTree import Element, ElementTree, parse, ParseError
 
+
 # TODO Add types to everything without and change existing types
 
-def main(verbose: bool, ddos: bool) -> None:
-    if ddos:
-        cwd_path: str = os.getcwd()
-        if not cwd_path.endswith("python_fuzzer"):
-            cwd_path = os.path.join(cwd_path, "python_fuzzer")
-        process_path: str = os.path.join(cwd_path, "executables", "ClientExample")
-        document_path: str = os.path.join(process_path, "Resources", "xml", "ProductionUddi", "OIOUBL_Invoice_v2p2.xml")
-        dosser: Ddos = Ddos()
-        dosser.generate(full_file_path=document_path)
-        dosser.run(process_path)
-    else:
-        # Get current working directory to create folders
-        cwd_path: str = os.getcwd()
-        if not cwd_path.endswith("python_fuzzer"):
-            cwd_path = os.path.join(cwd_path, "python_fuzzer")
-    
-        # Initialize the logger
-        # Maybe used for logging the good files that make it crash
-        logger_path: str = os.path.join(cwd_path, "log_files")
-        log: FeedbackLogger = FeedbackLogger(logger_path, verbose)
-    
-        # Initialize the runner
-        process_path: str = os.path.join(cwd_path, "executables", "ClientExample")
-        run: RaspRunner = RaspRunner(log, process_path, verbose)
-    
-        # Parse OIOUBL documents
-        document_path: str = os.path.join(cwd_path, "documents", "existing")
-        parser: DocumentParser = DocumentParser(document_path, verbose)
-        corpus_str, corpus = parser.load_corpus()
-    
-        # Initialize the mutator
-        mut: DocumentMutator = DocumentMutator(verbose)
-    
-        population_path: str = os.path.join(cwd_path, "documents", "population")
-        # Initialize and run the fuzzer
-        # fuzz: RaspFuzzer = RaspFuzzer(corpus_str, run, mut, log, verbose, parser, document_path, mutation_count=1)
-        # result = fuzz.multiple_runs(run_count=3)
-        # print(result)
-    
-        # Greybox fuzzer
-        fuzz: GreyboxFuzzer = GreyboxFuzzer(corpus, run, mut, log, PowerSchedule(), verbose, population_path, mutation_count=1)
-        result = fuzz.multiple_runs(run_count=1000000)
-        # print(result)
+def main(verbose: bool) -> None:
+    # Get current working directory to create folders
+    cwd_path: str = os.getcwd()
+    if not cwd_path.endswith("python_fuzzer"):
+        cwd_path = os.path.join(cwd_path, "python_fuzzer")
+
+    # Initialize the logger
+    # Maybe used for logging the good files that make it crash
+    logger_path: str = os.path.join(cwd_path, "log_files")
+    log: FeedbackLogger = FeedbackLogger(logger_path, verbose)
+
+    # Initialize the runner
+    process_path: str = os.path.join(cwd_path, "executables", "ClientExample")
+    run: RaspRunner = RaspRunner(log, process_path, verbose)
+
+    # Parse OIOUBL documents
+    document_path: str = os.path.join(cwd_path, "documents", "existing")
+    parser: DocumentParser = DocumentParser(document_path, verbose)
+    corpus_str, corpus = parser.load_corpus()
+
+    # Initialize the mutator
+    mut: DocumentMutator = DocumentMutator(verbose)
+
+    population_path: str = os.path.join(cwd_path, "documents", "population")
+    # Initialize and run the fuzzer
+    # fuzz: RaspFuzzer = RaspFuzzer(corpus_str, run, mut, log, verbose, parser, document_path, mutation_count=1)
+    # result = fuzz.multiple_runs(run_count=3)
+    # print(result)
+
+    # Greybox fuzzer
+    fuzz: GreyboxFuzzer = GreyboxFuzzer(corpus, run, mut, log, PowerSchedule(), verbose, population_path,
+                                        mutation_count=1)
+    result = fuzz.multiple_runs(run_count=5)
+    # print(result)
+
+
+# Method for setup ddos attack
+def ddos():
+    cwd_path: str = os.getcwd()
+    if not cwd_path.endswith("python_fuzzer"):
+        cwd_path = os.path.join(cwd_path, "python_fuzzer")
+    process_path: str = os.path.join(cwd_path, "executables", "ClientExample")
+    document_path: str = os.path.join(process_path, "Resources", "xml", "ProductionUddi", "OIOUBL_Invoice_v2p2.xml")
+    dosser: Ddos = Ddos()
+    dosser.generate(full_file_path=document_path)
+    dosser.run(process_path)
+
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description="Arguments for fuzzing harness")
@@ -72,4 +77,8 @@ if __name__ == '__main__':
 
     args = p.parse_args()
 
-    main(args.verbose, args.ddos)
+    # Run ddos code
+    if args.ddos:
+        ddos()
+    else:
+        main(args.verbose)
