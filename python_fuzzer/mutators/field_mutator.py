df76fa14a8442b0c5382d62ebf2ea5bc9401ab92
Fulei <emil.fulei@gmail.com>
2023-04-20 12:55:55 +0200

    Add and delete

--- python_fuzzer/mutators/field_mutator.py
+++ python_fuzzer/mutators/field_mutator.py
@@ -15,7 +15,12 @@ class FieldMutator(Mutator):
         # List mutator functions here
         self.mutators: List[Callable[[Any], Any]] = [self.replace_string_mutator,
                                                      self.replace_sub_mutator,
-                                                     self.replace_char_mutator]
+                                                     self.replace_char_mutator,
+                                                     self.delete_sub_mutator,
+                                                     self.delete_char_mutator,
+                                                     self.add_sub_mutator,
+                                                     self.add_char_mutator
+                                                     ]
 
         self.generators: List[Callable[[Any], Any]] = [TypeGenerator.make_string,
                                                        TypeGenerator.make_int,
@@ -54,11 +59,38 @@ class FieldMutator(Mutator):
         c: str = TypeGenerator.make_string()
 
         data = data[:start_pos] + c + data[end_pos:]
-        return c
+        return data
 
     def replace_char_mutator(self, data: str) -> str:
         start_pos: int = random.randint(0, len(data) - 1)
         c: str = TypeGenerator.make_char()
 
         data = data[:start_pos] + c + data[start_pos + 1:]
-        return c
+        return data
+
+    def delete_sub_mutator(self, data: str) -> str:
+        start_pos: int = random.randint(0, len(data) - 1)
+        end_pos: int = random.randint(start_pos, len(data) - 1)
+
+        data = data[:start_pos] + data[end_pos:]
+        return data
+
+    def delete_char_mutator(self, data: str) -> str:
+        start_pos: int = random.randint(0, len(data) - 1)
+
+        data = data[:start_pos] + data[start_pos + 1:]
+        return data
+
+    def add_sub_mutator(self, data: str) -> str:
+        start_pos: int = random.randint(0, len(data) - 1)
+        c: str = TypeGenerator.make_string()
+
+        data = data[:start_pos] + c + data[start_pos + 1:]
+        return data
+
+    def add_char_mutator(self, data: str) -> str:
+        start_pos: int = random.randint(0, len(data) - 1)
+        c: str = TypeGenerator.make_char()
+
+        data = data[:start_pos] + c + data[start_pos + 1:]
+        return data
