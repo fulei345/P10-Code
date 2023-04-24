44e86384646cad096b74cacc506ef86f40d6b23e
Fulei <emil.fulei@gmail.com>
2023-04-24 15:02:14 +0200

    lol

--- python_fuzzer/fuzzers/grey_fuzzer.py
+++ python_fuzzer/fuzzers/grey_fuzzer.py
@@ -60,8 +60,8 @@ class GreyboxFuzzer(Fuzzer):
         # Stacking: Apply multiple mutations to generate the candidate
         candidate = deepcopy(seed.data)
         num_mutations = random.randint(1, self.mutation_count)
-        # for _ in range(num_mutations):
-        #    candidate = self.mutator.mutate(candidate)
+        for _ in range(num_mutations):
+            candidate = self.mutator.mutate(candidate)
         return candidate
 
     def fuzz(self, inp: Any) -> ElementTree:
