12c3cc5799342fca3b4bd8552c3c45d1472c2a8c
Fulei <emil.fulei@gmail.com>
2023-04-25 08:51:05 +0200

    added interesting mutator and deleted genererator list

--- python_fuzzer/mutators/field_mutator.py
+++ python_fuzzer/mutators/field_mutator.py
@@ -8,6 +8,10 @@ import sys
 sys.path.append("..")
 from utils import TypeGenerator
 
+INTERESTING8 = [-128, -1, 0, 1, 16, 32, 64, 100, 127]
+INTERESTING16 = [0, 128, 255, 256, 512, 1000, 1024, 4096, 32767, 65535]
+INTERESTING32 = [0, 1, 32768, 65535, 65536, 100663045, 2147483647, 4294967295]
+
 
 class FieldMutator(Mutator):
     def __init__(self, verbose: bool) -> None:
@@ -19,15 +23,11 @@ class FieldMutator(Mutator):
                                                      self.delete_sub_mutator,
                                                      self.delete_char_mutator,
                                                      self.add_sub_mutator,
-                                                     self.add_char_mutator
+                                                     self.add_char_mutator,
+                                                     self.interesting8_mutator,
+                                                     self.interesting16_mutator,
+                                                     self.interesting32_mutator
                                                      ]
-
-        self.generators: List[Callable[[Any], Any]] = [TypeGenerator.make_string,
-                                                       TypeGenerator.make_int,
-                                                       TypeGenerator.make_float,
-                                                       TypeGenerator.make_bool,
-                                                       TypeGenerator.make_time,
-                                                       TypeGenerator.make_date]
         # self.dont_mutate: List[str] = ["CustomizationID",
         #                                "CopyIndicator", "FreeOfChargeIndicator", "CatalogueIndicator", "HazardousRiskIndicator"
         #                                "IssueDate", "TaxPointDate", "ActualDeliveryDate", "LatestDeliveryDate", "Date", "TaxPointDate"]
@@ -94,3 +94,15 @@ class FieldMutator(Mutator):
 
         data = data[:start_pos] + c + data[start_pos + 1:]
         return data
+
+    def interesting8_mutator(self, data: str) -> str:
+        data = random.choice(INTERESTING8)
+        return str(data)
+
+    def interesting16_mutator(self, data: str) -> str:
+        data = random.choice(INTERESTING16)
+        return str(data)
+
+    def interesting32_mutator(self, data: str) -> str:
+        data = random.choice(INTERESTING32)
+        return str(data)
