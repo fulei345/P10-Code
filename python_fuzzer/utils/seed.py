30cf3146c08191fc91c45d8cd66464b30659c23b
Fulei <emil.fulei@gmail.com>
2023-04-28 09:11:39 +0200

    Assign after

--- python_fuzzer/utils/seed.py
+++ python_fuzzer/utils/seed.py
@@ -6,15 +6,16 @@ class Seed:
     """Represent an input with additional attributes"""
 
     # Type Should probably be ElementTree
-    def __init__(self, data: Any, coverage: set, outcome: str, result: str) -> None:
+    def __init__(self, data: Any) -> None:
         """Initialize from seed data"""
         self.data: Any = data
 
         # These will be needed for advanced power schedules
-        self.coverage: Set[str] = coverage
-        self.outcome: str = outcome
-        self.result: str = result
+        self.coverage: Set[str] = set()
+        self.outcome: str = ""
+        self.result: str = ""
         self.energy = 0.0
+        self.chosen_count = 0
 
     def __str__(self) -> str:
         """Returns data as string representation of the seed"""
