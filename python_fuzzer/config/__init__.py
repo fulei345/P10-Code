582e1825197cb75878793e10f44b6e324593a1e9
Fulei <emil.fulei@gmail.com>
2023-04-28 11:26:42 +0200

    adde prob

---
+++ python_fuzzer/config/__init__.py
@@ -0,0 +1,5 @@
+from .settings import MUTATION_COUNT
+from .settings import IF_PROB
+from .settings import PS_PROB
+
+__all__ = ["MUTATION_COUNT", "IF_PROB", "PS_PROB"]
---
+++ python_fuzzer/config/settings.py
@@ -0,0 +1,12 @@
+
+
+# Number of mutations done before sending
+MUTATION_COUNT = 1
+
+# Probability of being uniform or favor valid
+PS_PROB = 0.5
+
+# Pobability of being inserted in the whole document or in parent
+IF_PROB = 0.5
+
+
