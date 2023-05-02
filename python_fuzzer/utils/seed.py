41c26e2c26a4482bcf0156fe256302f484e379ea
Fulei <emil.fulei@gmail.com>
2023-05-02 14:50:04 +0200

    lol

--- python_fuzzer/utils/seed.py
+++ python_fuzzer/utils/seed.py
@@ -16,6 +16,7 @@ class Seed:
         self.result: str = ""
         self.energy = 0.0
         self.chosen_count = 0
+        self.population_name = "first"
 
     def __str__(self) -> str:
         """Returns data as string representation of the seed"""
