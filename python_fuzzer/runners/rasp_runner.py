e4bb0e34ad5656dd7df082e2ccd93fcedeaa1d87
Fulei <emil.fulei@gmail.com>
2023-05-04 10:54:12 +0200

    unknown log og baseret på response recieved

--- python_fuzzer/runners/rasp_runner.py
+++ python_fuzzer/runners/rasp_runner.py
@@ -37,8 +37,8 @@ class RaspRunner(Runner):
     def run(self, document: ElementTree, filename: str) -> Tuple[Any, str, List[str]]:
         document_path = join(self.executable_path, "Resources", "xml", "ProductionUddi", filename)
         document.write(document_path, encoding="utf-8", xml_declaration=True)
-        message, code, code_coverage = self.start_process(document_path)
-        return message, code, code_coverage
+        message, outcome, code_coverage = self.start_process(document_path)
+        return message, outcome, code_coverage
 
     def start_process(self, doc_path: str) -> Tuple[str, str, List[str]]:
         # Initialize coverage if something goes wrong
@@ -71,46 +71,40 @@ class RaspRunner(Runner):
         self.code_coverage = findall(r"BLOCK:\d+", standard_out)
 
         # finds the second instance of the substring, which is the start of the error message
-        erro_index = standard_out.find("dk.gov.oiosi", standard_out.find("dk.gov.oiosi") + 1)
+        # erro_index = standard_out.find("dk.gov.oiosi", standard_out.find("dk.gov.oiosi") + 1)
 
-        # Check if we found it the start of an error
-        if -1 != erro_index:
+        if self.verbose:
+            print(standard_out)
 
-            # Find the fault message in the standard out
-            fault_message = standard_out[erro_index:]
-
-            # Regex to find E-RSP num
-            ersp_num = findall(r"E-RSP\d+", fault_message)
-            if len(ersp_num) > 1:
-                # Log forskellige, hvis der er flere
-                if ersp_num[1] == "E-RSP15324":
-                    return fault_message, ersp_num[1], self.code_coverage
-
-            # If it is E-RSP and not already seen number
-            if len(ersp_num) > 0 and ersp_num[0] not in self.ersp_nums:
-                self.ersp_nums.append(ersp_num[0])
-
-                ## F fault code
-                # f_num = search(r"\[F-\w+\]", fault_message)
-                # if f_num != None and f_num.group(0) not in self.ersp_nums:
-                # self.ersp_nums.append(f_num.group(0))
-
-                if self.verbose:
-                    print(fault_message)
-
-                # If it was an E-RSP fault
-                return fault_message, ersp_num[0], self.code_coverage
-
-            if self.verbose:
-                print(fault_message)
-            return fault_message, self.UNKNOWN, self.code_coverage
-
-        else:
-            if self.verbose:
-                print(standard_out)
-            # If we did not find any error
+         
+        if "Response received" in standard_out:
+            # It was succesful
             return standard_out, self.PASS, self.code_coverage
+        # It did not pass
+        else:
+            # # Kunne også tjekke F fault code
+            # f_num = search(r"\[F-\w+\]", fault_message)
+            # if f_num != None and f_num.group(0) not in self.ersp_nums:
+            # self.ersp_nums.append(f_num.group(0))
+            
+            # Regex to find E-RSP num
+            ersp_num = findall(r"E-RSP\d+", standard_out)
 
+            if len(ersp_num) == 0:
+                # We don't know
+                # Denne her exception har ikke et nummer
+                if "System.Xml.XmlException" in standard_out:
+                    return standard_out, self.EXCEPTION, self.code_coverage
+                return standard_out, self.UNKNOWN, self.code_coverage
+
+            # Loop all E-RSP nums
+            outcome = ""
+            for num in ersp_num:
+                outcome += "-"
+                outcome += num
+
+            # If it was an E-RSP fault
+            return standard_out, outcome, self.code_coverage
 
 
 if __name__ == '__main__':
