effb0bd42bd8062734ec11fc2a2779dafe09476b
Fulei <emil.fulei@gmail.com>
2023-05-12 09:56:59 +0200

    max pop replace count

--- python_fuzzer/config/settings.py
+++ python_fuzzer/config/settings.py
@@ -1,7 +1,7 @@
 
 
 # Number of mutations done before sending
-MUTATION_COUNT = 1
+MUTATION_COUNT = 5
 
 # If power scheduler should use weighted energy
 WEIGHTED_PS = True
@@ -9,7 +9,12 @@ WEIGHTED_PS = True
 # Pobability of being inserted in the whole document or in parent
 IF_PROB = 0.5
 
+# The number of iterations before the seeds is getting replaced
+REPLACE_COUNT = 5
+
+# The max seeds in population
+MAX_POPULATION = 30
+
 # Wether if adding to population depends on coverage or outcome
 # True means coverage
 COVERAGE_BASED = True
-
