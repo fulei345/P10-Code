cc0005679a854e94204353ad3c5145eeb962e793
faje18 <faje18@student.aau.dk>
2023-05-14 00:03:14 +0200

    option for probability of making optional field

--- python_fuzzer/config/__init__.py
+++ python_fuzzer/config/__init__.py
@@ -2,5 +2,6 @@ from .settings import MUTATION_COUNT
 from .settings import IF_PROB
 from .settings import WEIGHTED_PS
 from .settings import COVERAGE_BASED
+from .settings import OPT_PROP
 
-__all__ = ["MUTATION_COUNT", "IF_PROB", "WEIGHTED_PS", "COVERAGE_BASED"]
+__all__ = ["MUTATION_COUNT", "IF_PROB", "WEIGHTED_PS", "COVERAGE_BASED", "OPT_PROP"]
--- python_fuzzer/config/settings.py
+++ python_fuzzer/config/settings.py
@@ -9,6 +9,9 @@ WEIGHTED_PS = True
 # Pobability of being inserted in the whole document or in parent
 IF_PROB = 0.5
 
+# probability of and optional field being made
+OPT_PROP = 0.5
+
 # Wether if adding to population depends on coverage or outcome
 # True means coverage
 COVERAGE_BASED = True
