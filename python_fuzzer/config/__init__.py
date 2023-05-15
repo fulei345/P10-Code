e3234169f70eb27171541ef4c0a62830b825cf85
Fulei <emil.fulei@gmail.com>
2023-05-15 14:24:00 +0200

    chanded replace count to matter

--- python_fuzzer/config/__init__.py
+++ python_fuzzer/config/__init__.py
@@ -2,5 +2,6 @@ from .settings import MUTATION_COUNT
 from .settings import IF_PROB
 from .settings import WEIGHTED_PS
 from .settings import MAX_DICT
+from .settings import REPLACE_COUNT
 
-__all__ = ["MUTATION_COUNT", "IF_PROB", "WEIGHTED_PS", "MAX_POPULATION", "MAX_DICT"]
+__all__ = ["MUTATION_COUNT", "IF_PROB", "WEIGHTED_PS", "REPLACE_COUNT", "MAX_DICT"]
