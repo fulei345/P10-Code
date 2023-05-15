0135229ebd3a8a06c55bb79672d8baf3edd94ffd
fulei <emil.fulei@gmail.com>
2023-05-15 13:58:30 +0200

    deleted max popluation, replace count 10

--- python_fuzzer/config/settings.py
+++ python_fuzzer/config/settings.py
@@ -4,16 +4,13 @@
 MUTATION_COUNT = 5
 
 # If power scheduler should use weighted energy
-WEIGHTED_PS = False
+WEIGHTED_PS = True
 
 # Pobability of being inserted in the whole document or in parent
 IF_PROB = 0.5
 
-# The number of iterations before the seeds is getting replaced
-REPLACE_COUNT = 5
-
-# The max seeds in population
-MAX_POPULATION = 40
+# The number of times that seed is send before the seeds is getting replaced
+REPLACE_COUNT = 10
 
 # Max number of different seed type in population
 MAX_DICT = {"SCHEMA": 5, "PASS": 5, "SCHEMATRON": 10, "UNKNOWN": 100, "FAIL": 100, "XML": 5}
