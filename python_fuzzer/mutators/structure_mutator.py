20bebdf8c04d8d2252fbcfb6d663f021f96163fc
Fulei <emil.fulei@gmail.com>
2023-05-26 14:28:42 +0200

    forgot list

--- python_fuzzer/mutators/structure_mutator.py
+++ python_fuzzer/mutators/structure_mutator.py
@@ -163,7 +163,7 @@ class StructureMutator(Mutator):
         elif field_type == bytes:
             elem.text = TypeGenerator.make_string() #TODO change this (look at oioubl documentation for attachement binary object)
         elif field_type == float:
-            float_mut = random.choice(TypeGenerator.make_float, TypeGenerator.make_float_thousands)
+            float_mut = random.choice([TypeGenerator.make_float, TypeGenerator.make_float_thousands])
             elem.text = float_mut
         else:
             #change namespace to class namespace
