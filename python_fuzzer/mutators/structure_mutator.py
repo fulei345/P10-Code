4b571a7799212e214135bb6860b01ab9b0baab77
Fulei <emil.fulei@gmail.com>
2023-05-26 12:48:18 +0200

    added thousands to structure mutatot

--- python_fuzzer/mutators/structure_mutator.py
+++ python_fuzzer/mutators/structure_mutator.py
@@ -163,7 +163,8 @@ class StructureMutator(Mutator):
         elif field_type == bytes:
             elem.text = TypeGenerator.make_string() #TODO change this (look at oioubl documentation for attachement binary object)
         elif field_type == float:
-            elem.text = TypeGenerator.make_float()
+            float_mut = random.choice(TypeGenerator.make_float, TypeGenerator.make_float_thousands)
+            elem.text = float_mut
         else:
             #change namespace to class namespace
             elem = Element("{" + "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" + "}" + field.name)
