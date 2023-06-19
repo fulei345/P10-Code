261c49ab792f0af1f7dc1d3b73f90b38c4d04e40
faje18 <faje18@student.aau.dk>
2023-06-19 12:05:05 +0200

    runners

--- python_fuzzer/runners/rasp_runner.py
+++ python_fuzzer/runners/rasp_runner.py
@@ -1,4 +1,4 @@
-from typing import Any, Tuple, List
+from typing import Tuple
 from subprocess import run
 from os import getcwd
 from os.path import join
@@ -14,11 +14,11 @@ else:
 
 import sys
 sys.path.append("..")
-from loggers import FeedbackLogger
+from loggers import Logger
 
 
 class RaspRunner(Runner):
-    def __init__(self, log: FeedbackLogger, path: str, verbose: bool) -> None:
+    def __init__(self, log: Logger, path: str, verbose: bool) -> None:
         self.PASS: str = 'PASS'
         self.FAIL: str = 'FAIL'
         self.SCHEMA: str = 'SCHEMA'
@@ -26,20 +26,17 @@ class RaspRunner(Runner):
         self.XML: str = 'XML'
         self.UNKNOWN: str = 'UNKNOWN'
 
-        self.logger: FeedbackLogger = log
-        self.ersp_nums: List[str] = []
-
+        self.logger: Logger = log
         self.executable_path: str = path
         self.verbose: bool = verbose
         self.code_coverage = []
 
-        # Just for E-RSP15324 and returncode not zero
-        self.count = 0 
-
-    def run(self, document: ElementTree, filename: str) -> Tuple[Any, str, List[str]]:
+    def run(self, document: ElementTree, filename: str) -> Tuple[str, str]:
+        #make path for document and write the document there, then run process with path provided
         document_path = join(self.executable_path, "Resources", "xml", "ProductionUddi", filename)
         document.write(document_path, encoding="utf-8", xml_declaration=True)
         message, outcome = self.start_process(document_path)
+        #remove file from executable folder
         os.remove(document_path)
         return message, outcome
 
@@ -47,52 +44,41 @@ class RaspRunner(Runner):
         # Initialize coverage if something goes wrong
         self.code_coverage = {}
         try:
-            # Input is the options chosen in the Client
+            # run client example with specified document path
             process = run(["dk.gov.oiosi.samples.ClientExample.exe", doc_path],
                           shell=True,
                           cwd=self.executable_path,
                           timeout=30,
                           capture_output=True)
 
+            # if the process failed during run, handles the error
             if process.returncode != 0:
                 standard_error = process.stderr.decode("utf-8", errors="replace")
                 if self.verbose:
                     print(standard_error)
                 return standard_error, self.FAIL
-
+            # if it suceeded, handle feedback
             elif process.returncode == 0:
-                # TODO find better way to handle decode error for ø (+ æ and å, i suppose)
+                # handle decode error for ø, æ and å by replacing them
                 standard_out = process.stdout.decode("utf-8", errors="replace")
                 return self.handle_feedback(standard_out)
         except Exception as err:
             traceback.print_exc()
             return str(traceback.format_exception(err)), self.FAIL
 
-    def handle_feedback(self, standard_out: str) -> Tuple[str, str, List[str]]:
+    def handle_feedback(self, standard_out: str) -> Tuple[str, str]:
         # Find code blocks for code coverage
         self.code_coverage = findall(r"BLOCK:\d+", standard_out)
-
-        # finds the second instance of the substring, which is the start of the error message
-        # erro_index = standard_out.find("dk.gov.oiosi", standard_out.find("dk.gov.oiosi") + 1)
 
         if self.verbose:
             print(standard_out)
 
-         
         if "Response received" in standard_out:
             # It was succesful
             return standard_out, self.PASS
         # It did not pass
         else:
-            # # Kunne også tjekke F fault code
-            # f_num = search(r"\[F-\w+\]", fault_message)
-            # if f_num != None and f_num.group(0) not in self.ersp_nums:
-            # self.ersp_nums.append(f_num.group(0))
-            
-            # Regex to find E-RSP num
-            # ersp_num = findall(r"E-RSP\d+", standard_out)
-
-
+            # list of known xml exception returned from client
             xml_list = ["NoDocumentTypeFoundFromXmlDocumentException",
                         "System.Xml.XmlException",
                         "SearchForDocumentTypeFromXmlDocumentFailedException",]
@@ -107,16 +93,17 @@ class RaspRunner(Runner):
                 return standard_out, self.SCHEMATRON
 
             else:
+                #checks if its one of the known xml exceptions
                 for s in xml_list:
                     if s in standard_out:
                         return standard_out, self.XML
+                #else its unknown
                 return standard_out, self.UNKNOWN
-        
             
 
 if __name__ == '__main__':
     cwd_path = getcwd()
     # Get path to the folder of the ClientExample
     process_path: str = join(cwd_path, "..", "executables", "ClientExample")
-    logger: FeedbackLogger = FeedbackLogger(cwd_path, True)
+    logger: Logger = Logger(cwd_path, True)
     runner: RaspRunner = RaspRunner(logger, process_path, True)
