5b0e52797b353128f1c5e1449f3f30446593bf3a
faje18 <faje18@student.aau.dk>
2023-06-22 11:37:43 +0200

    attributes_list in init

--- python_fuzzer/codelists/__init__.py
+++ python_fuzzer/codelists/__init__.py
@@ -1,3 +1,3 @@
-from .invoice_codelist import names_list, codelist_list
+from .invoice_codelist import names_list, codelist_list, attributes_list
 
-__all__ = ["names_list", "codelist_list"]
+__all__ = ["names_list", "codelist_list", "attributes_list"]
