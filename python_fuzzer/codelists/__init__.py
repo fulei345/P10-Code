9622d1ac64bdf2b06d5e72d90664539a3295a2db
fulei <emil.fulei@gmail.com>
2023-06-22 11:48:36 +0200

    makelist makes attributes

--- python_fuzzer/codelists/__init__.py
+++ python_fuzzer/codelists/__init__.py
@@ -1,3 +1,3 @@
-from .invoice_codelist import names_list, codelist_list
+from .invoice_codelist import names_list, codelist_list, attributes_list, attributes__name_list
 
-__all__ = ["names_list", "codelist_list"]
+__all__ = ["names_list", "codelist_list", "attributes_list", "attributes__name_list"]
