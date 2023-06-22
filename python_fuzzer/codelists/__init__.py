7496c0d7f418edffb5da1c14d252d71c4a006b1a
faje18 <faje18@student.aau.dk>
2023-06-22 12:58:09 +0200

    removed list from init again

--- python_fuzzer/codelists/__init__.py
+++ python_fuzzer/codelists/__init__.py
@@ -1,3 +1,3 @@
-from .invoice_codelist import names_list, codelist_list, attributes_list, attributes_names_list
+from .invoice_codelist import names_list, codelist_list
 
-__all__ = ["names_list", "codelist_list", "attributes_list", "attributes_names_list"]
+__all__ = ["names_list", "codelist_list"]
