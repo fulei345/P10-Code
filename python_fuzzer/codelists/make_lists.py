5703ab856d1e1b4e600fa09e103ed4b482b30fac
fulei <emil.fulei@gmail.com>
2023-06-23 11:07:11 +0200

    update codelist

--- python_fuzzer/codelists/make_lists.py
+++ python_fuzzer/codelists/make_lists.py
@@ -6,26 +6,28 @@ names_list = []
 codelist_list = []
 
 Lines = file.readlines()
- 
+
 found_only_start_line = False
 for line in Lines:
     if found_only_start_line:
         select = line.split("\'")
         select = select[1]
         select = select.split(",")
-        if len(select) != 1:
+        # IF there is empty at both ends trim that
+        if select [0] == "" and select [-1] == "":
             select = select[1:-1]
         codelist_list.append(select)
         found_only_start_line = False
     else:
         if "name=" in line and "select=" in line:
+            name = line.split("\"")[1]
             select = line.split("\'")
             select = select[1]
             select = select.split(",")
-            if len(select) != 1:
+            # IF there is empty at both ends trim that
+            if select [0]== "" and select[-1] == "":
                 select = select[1:-1]
             codelist_list.append(select)
-            name = line.split("\"")[1]
             names_list.append(name)
         else:
             name = line.split("\"")[1]
@@ -38,6 +40,8 @@ print(codelist_list)
 
 
 with open("python_fuzzer//codelists//schematron.txt", "w", encoding="utf-8") as file:
+    file.write("from typing import List\n\n")
+
     file.write("names_list: List[str] = [")
     name_write_to = ["\"" + name + "\",\n" for name in names_list]
     file.writelines(name_write_to)
